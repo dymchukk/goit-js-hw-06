3f9945fa2679b380249b30bf14ccdcea14b61f88
Dymchuk Ekateryna <dymchukkat@gmail.com>
2022-02-01 19:17:40 +0200

    commit

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -17,18 +17,16 @@ const dekrBtn = document.querySelector('#counter').firstElementChild;
 const inkrBtn = document.querySelector('#counter').lastElementChild;
 
 inkrBtn.addEventListener('click', event => {
-  // console.log("Вешаю слушателя события на целевую кнопку");
   valueEl += 1;
 
   counterValue.innerText = valueEl;
 });
 
 dekrBtn.addEventListener('click', event => {
-  // console.log("Снимаю слушателя события с целевой кнопки");
   valueEl -= 1;
 
   counterValue.innerText = valueEl;
-  // console.log("counterValue", counterValue);
+
 });
 
 
