ae11229d7a34a27f4819a0f716edcb144042ab0a
Dymchuk Ekateryna <dymchukkat@gmail.com>
2022-02-02 14:47:34 +0200

    git commit

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -18,5 +18,21 @@
 Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
 */
 
+const formEl = document.querySelector("form");
 
+formEl.addEventListener("submit", onButtonSubmit);
+
+function onButtonSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  console.log({ email: email.value, password: password.value });
+  event.currentTarget.reset(); 
+}
 
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,9 +1,65 @@
+/*Напиши скрипт создания и очистки коллекции элементов. Пользователь вводит количество элементов в input и нажимает кнопку Создать, после чего рендерится коллекция. При нажатии на кнопку Очистить, коллекция элементов очищается.
+
+<div id="controls">
+  <input type="number" min="1" max="100" step="1" />
+  <button type="button" data-create>Create</button>
+  <button type="button" data-destroy>Destroy</button>
+</div>
+
+<div id="boxes"></div>
+Создай функцию createBoxes(amount), которая принимает один параметр - число. Функция создает столько <div>, сколько указано в amount и добавляет их в div#boxes.
+
+Размеры самого первого <div> - 30px на 30px.
+Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
+Все элементы должены иметь случайный цвет фона в формате HEX. Используй готовую функцию getRandomHexColor для получения цвета.
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+Создай функцию destroyBoxes(), которая очищает содержимое div#boxes, тем самым удаляя все созданные элементы.*/
+
+
+
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+let initialSizeOfBox = 30;
+const refs = {
+  createBtn: document.querySelector('[data-create]'),
+  destroyBtn: document.querySelector('[data-destroy]'),
+  boxesParent: document.querySelector('#boxes'),
+  inputEl: document.querySelector('#controls').firstElementChild,
+};
+refs.createBtn.addEventListener('click', createBtnListener);
+refs.destroyBtn.addEventListener('click', destroyBoxes);
 
+function createBtnListener() {
+  const amountOfBoxes = createBoxes(refs.inputEl.value);
+  refs.boxesParent.append(...amountOfBoxes);
+}
 
-/*
-const input = document.querySelector('input')
-console.log(input.value)*/
+function createBoxes(amount) {
+  let sizeOfBox = 0;
+  const boxElements = [];
+  for (let i = 0; i < amount; i += 1) {
+    const boxEl = document.createElement('div');
+    boxEl.style.backgroundColor = getRandomHexColor();
+    boxEl.style.display = 'block';
+    sizeOfBox = initialSizeOfBox + i * 10;
+    boxEl.style.width = `${sizeOfBox}px`;
+    boxEl.style.height = `${sizeOfBox}px`;
+    boxElements.push(boxEl);
+  }
+  initialSizeOfBox = sizeOfBox + 10;
+  return boxElements;
+}
+
+function destroyBoxes() {
+  refs.boxesParent.innerHTML = '';
+  initialSizeOfBox = 30;
+}
