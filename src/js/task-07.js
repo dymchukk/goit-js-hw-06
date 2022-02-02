273936f8c275a4cb0d4bc4fee758bfa3da9d9c0f
Dymchuk Ekateryna <dymchukkat@gmail.com>
2022-02-02 10:04:16 +0200

    commit

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -12,7 +12,7 @@ const textEl = document.querySelector("#text");
 
 const inputEl = document.querySelector("#font-size-control");
 
-inputEl.addEventListener("input", (e) => {
+inputEl.addEventListener("input", (elem) => {
   const size = inputEl.value;
   // console.log(size);
 
